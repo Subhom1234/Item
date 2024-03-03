14193a8917369b40e80b2819870d0b0c503feb34
Subhom1234 <147125659+Subhom1234@users.noreply.github.com>
2024-03-04 00:01:16 +0530

    Update main1.js

--- main1.js
+++ main1.js
@@ -47,4 +47,22 @@ function zoomOut() {
             // Remove the link from the body after the download starts
             document.body.removeChild(downloadLink);
         }
+function downloadAPK2() {
+            // Replace 'your_app.apk' with the actual name of your APK file
+            var apkUrl = 'Home.apk';
+            
+            // Create a link element
+            var downloadLink = document.createElement('a');
+            downloadLink.href = apkUrl;
+            downloadLink.download = 'your_app.apk';
+
+            // Append the link to the body
+            document.body.appendChild(downloadLink);
+
+            // Trigger a click on the link to start the download
+            downloadLink.click();
+
+            // Remove the link from the body after the download starts
+            document.body.removeChild(downloadLink);
+}
     
